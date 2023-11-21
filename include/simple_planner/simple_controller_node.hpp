872ac170b691331d2f73e18a0262bd40bef4cc15
Michael Hubbertz <michael.hubbertz@rwth-aachen.de>
2023-11-21 16:20:54 +0100

    Changed controller implementation to PID longitudinal and lateral control

--- include/simple_planner/simple_controller_node.hpp
+++ include/simple_planner/simple_controller_node.hpp
@@ -11,6 +11,7 @@
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/twist.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <carla_msgs/msg/carla_egovehicle_control.hpp>
 
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
@@ -39,19 +40,24 @@ class SimpleControllerNode : public rclcpp::Node {
   void trajectoryCallback(const trajectory_interfaces::msg::Trajectory::UniquePtr msg);
   void publishTimerCallback();
 
+  double longitudinalControlStep(double current_velocity, double target_velocity);
+  double lateralControlStep(double current_yaw, double target_yaw);
+
   void trajectoryToCarlaCtrl(trajectory_interfaces::msg::Trajectory tra);
   bool linearInterpolation(const std::vector<double>& X, const std::vector<double>& Y, const double& desired_x, double& output_y);
 
  private:
 
-  std::unique_ptr<tf2_ros::Buffer> tf2_buffer_;
-  std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;
+  // std::unique_ptr<tf2_ros::Buffer> tf2_buffer_;
+  // std::shared_ptr<tf2_ros::TransformListener> tf2_listener_;
 
   rclcpp::Subscription<perception_interfaces::msg::EgoData>::SharedPtr sub_egoData_;
   rclcpp::Subscription<trajectory_interfaces::msg::Trajectory>::SharedPtr sub_trajectory_;
 
-  rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pub_pose_;
-  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_twist_;
+  // rclcpp::Publisher<geometry_msgs::msg::Pose>::SharedPtr pub_pose_;
+  // rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_twist_;
+
+  rclcpp::Publisher<carla_msgs::msg::CarlaEgoVehicleControl>::SharedPtr pub_ctrl_;
 
   rclcpp::TimerBase::SharedPtr publish_timer_;
 
@@ -59,11 +65,33 @@ class SimpleControllerNode : public rclcpp::Node {
   trajectory_interfaces::msg::Trajectory trajectory_;
 
   double pub_duration_;
-  geometry_msgs::msg::Pose recent_pose_;
+  // geometry_msgs::msg::Pose recent_pose_;
   
   bool ego_data_init_ = false;
   bool trajectory_init_ = false;
-  bool recent_pose_init_ = false; 
+  // bool recent_pose_init_ = false; 
+
+
+  // PID Controller
+  
+  // Longitudinal Controller Parameters:
+  double p_long = 0.206;
+  double i_long = 0.0206;
+  double d_long = 0.515;
+
+  // Lateral Controller Parameters:
+  double p_lat = 0.9;
+  double i_lat = 0.0;
+  double d_lat = 0.0;
+
+  // PID Controller state variables
+  double error_long = 0.0;
+  double error_integral_long = 0.0;
+  double error_derivative_long = 0.0;
+  double error_lat = 0.0;
+  double error_integral_lat = 0.0;
+  double error_derivative_lat = 0.0;
+
 };
 
 
--- src/simple_controller_node.cpp
+++ src/simple_controller_node.cpp
@@ -22,6 +22,7 @@ const std::string SimpleControllerNode::kEgoDataTopic = "~/ego_data_topic";
 const std::string SimpleControllerNode::kTrajectoryTopic = "~/trajectory_topic";
 const std::string SimpleControllerNode::kOutputPose = "/carla/ego_vehicle/control/set_transform";
 const std::string SimpleControllerNode::kOutputTwist = "/carla/ego_vehicle/control/set_target_velocity";
+const std::string SimpleControllerNode::kOutputCtrl = "/carla/ego_vehicle/control/ToDo";
 
 
 /**
@@ -40,8 +41,8 @@ SimpleControllerNode::SimpleControllerNode() : Node("simple_controller_node") {
  */
 void SimpleControllerNode::setup() {
 
-  tf2_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-  tf2_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
+  // tf2_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+  // tf2_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf2_buffer_);
 
   // create subscriber for egoData
   sub_egoData_ =
@@ -58,10 +59,13 @@ void SimpleControllerNode::setup() {
   RCLCPP_INFO(this->get_logger(), "Subscribed to '%s'", sub_trajectory_->get_topic_name());
 
   // create publisher for Pose and Twist (pass to carla topics)
-  pub_pose_ = this->create_publisher<geometry_msgs::msg::Pose>(kOutputPose, 10);
-  RCLCPP_INFO(this->get_logger(), "Publishing to '%s'", pub_pose_->get_topic_name());
-  pub_twist_ = this->create_publisher<geometry_msgs::msg::Twist>(kOutputTwist, 10);
-  RCLCPP_INFO(this->get_logger(), "Publishing to '%s'", pub_twist_->get_topic_name());
+  // pub_pose_ = this->create_publisher<geometry_msgs::msg::Pose>(kOutputPose, 10);
+  // RCLCPP_INFO(this->get_logger(), "Publishing to '%s'", pub_pose_->get_topic_name());
+  // pub_twist_ = this->create_publisher<geometry_msgs::msg::Twist>(kOutputTwist, 10);
+  // RCLCPP_INFO(this->get_logger(), "Publishing to '%s'", pub_twist_->get_topic_name());
+
+  // create publisher for Ego Vehicle Control message (pass to carla topic)
+  pub_ctrl_ = this->create_publisher<carla_msgs::msg::CarlaEgoVehicleControl>(kOutputCtrl, 10);
 
   pub_duration_ = 1.0/100.0;
 
@@ -142,68 +146,130 @@ void SimpleControllerNode::trajectoryToCarlaCtrl(const trajectory_interfaces::ms
   if(!linearInterpolation(TIME, X, des_time, x_tgt)) return;
   if(!linearInterpolation(TIME, Y, des_time, y_tgt)) return;
   if(!linearInterpolation(TIME, THETA, des_time, theta_tgt)) return;
-  
-  // Wrap interpolations into pose
-  geometry_msgs::msg::PoseStamped pose_bl;
-  pose_bl.header.stamp = trajectory_.header.stamp;
-  pose_bl.header.frame_id = "base_link";
-  pose_bl.pose.position.x = x_tgt;
-  pose_bl.pose.position.y = y_tgt;
 
-  // Set yaw for target pose (in base_link frame)
-  tf2::Quaternion quat_tf;
-  quat_tf.setRPY(0, 0, theta_tgt);
-  pose_bl.pose.orientation = tf2::toMsg(quat_tf);
 
-  // Get transform from base_link to carla_map frame
-  auto timeout = rclcpp::Duration::from_seconds(1.0);
-  geometry_msgs::msg::TransformStamped base_link_to_carla_map_tf;
-  try {
-    base_link_to_carla_map_tf = tf2_buffer_->lookupTransform("carla_map", pose_bl.header.frame_id, pose_bl.header.stamp, timeout);
-  } catch (tf2::TransformException& ex) {
-    RCLCPP_WARN(this->get_logger(), "Tranformation from %s to 'carla_map' is not available", pose_bl.header.frame_id.c_str());
-    return;
-  }
+  // Implementation to publish steering angle and throttle/brake messages
+  carla_msgs::msg::CarlaEgoVehicleControl ctrl_msg;
 
-  // Transform pose from base_link to carla_map frame
-  geometry_msgs::msg::PoseStamped pose_map;
-  tf2::doTransform(pose_bl, pose_map, base_link_to_carla_map_tf);
+  // set header
+  ctrl_msg.header.stamp = trajectory_.header.stamp;
+  ctrl_msg.header.frame_id = "base_link";
 
-  // Set velocity of pose by backward differences
-  geometry_msgs::msg::Twist twist;
-  if (!recent_pose_init_) {
-    // If no recent pose exists, set velocity to zero (only for very first pose)
-    twist.linear.x = 0;
-    twist.linear.y = 0;
-    twist.linear.z = 0;
-    twist.angular.x = 0;
-    twist.angular.y = 0;
-    twist.angular.z = 0;
-    recent_pose_init_ = true;
+  // fetch current ego pose
+  geometry_msgs::msg::Pose current_pose = perception_interfaces::object_access::getPose(ego_data_);
+
+  // set longitudinal control (throttle and brake)
+  double long_output = longitudinalControlStep(perception_interfaces::object_access::getV(ego_data_), v_tgt);
+
+  if (long_output >= 0.0){
+    ctrl_msg.throttle = long_output;
+    ctrl_msg.brake = 0.0;
   }
   else {
-    // Take backwards differences to derive estimated velocites (only set v_x, v_y and yaw rate)
-    twist.linear.x = (pose_map.pose.position.x - recent_pose_.position.x) / pub_duration_; // v_x
-    twist.linear.y = (pose_map.pose.position.y - recent_pose_.position.y) / pub_duration_; // v_y
-    twist.linear.z = 0;
+    ctrl_msg.throttle = 0.0;
+    ctrl_msg.brake = (-1) * long_output;
+  }
 
-    twist.angular.x = 0;
-    twist.angular.y = 0;
-    double current_roll, current_pitch, current_yaw, recent_roll, recent_pitch, recent_yaw;
-    tf2::Quaternion q_current(pose_map.pose.orientation.x, pose_map.pose.orientation.y, pose_map.pose.orientation.z, pose_map.pose.orientation.w);
-    tf2::Matrix3x3 m_current(q_current);
-    m_current.getRPY(current_roll, current_pitch, current_yaw);
-    tf2::Quaternion q_recent(recent_pose_.orientation.x, recent_pose_.orientation.y, recent_pose_.orientation.z, recent_pose_.orientation.w);
-    tf2::Matrix3x3 m_recent(q_recent);
-    m_recent.getRPY(recent_roll, recent_pitch, recent_yaw);
-    twist.angular.z = (current_yaw - recent_yaw) / pub_duration_ ; // yaw rate
-  }  
+  // set lateral control (steering angle)
+  double target_yaw = std::atan2(y_tgt, x_tgt); // Yaw angle to get to the target from current ego pose, NOT yaw angle of target pose!
+  double lat_output = lateralControlStep(perception_interfaces::object_access::getYaw(ego_data_), target_yaw);
+  ctrl_msg.steer = lat_output;
 
-  // Publish pose and twist to carla
-  pub_pose_->publish(pose_map.pose);
-  recent_pose_ = pose_map.pose;
-  pub_twist_->publish(twist);
-  RCLCPP_DEBUG(this->get_logger(), "Published pose and twist to Carla!");
+  // set other states that are not relevant
+  control.hand_brake = false;
+  control.reverse = false;
+  control.manual_gear_shift = false;
+
+  // publish control message
+  pub_ctrl_->publish(ctrl_msg);
+
+
+  // Implementation to publish pose and twist messages
+  
+  // // Wrap interpolations into pose
+  // geometry_msgs::msg::PoseStamped pose_bl;
+  // pose_bl.header.stamp = trajectory_.header.stamp;
+  // pose_bl.header.frame_id = "base_link";
+  // pose_bl.pose.position.x = x_tgt;
+  // pose_bl.pose.position.y = y_tgt;
+
+  // // Set yaw for target pose (in base_link frame)
+  // tf2::Quaternion quat_tf;
+  // quat_tf.setRPY(0, 0, theta_tgt);
+  // pose_bl.pose.orientation = tf2::toMsg(quat_tf);
+
+  // // Get transform from base_link to carla_map frame
+  // auto timeout = rclcpp::Duration::from_seconds(1.0);
+  // geometry_msgs::msg::TransformStamped base_link_to_carla_map_tf;
+  // try {
+  //   base_link_to_carla_map_tf = tf2_buffer_->lookupTransform("carla_map", pose_bl.header.frame_id, pose_bl.header.stamp, timeout);
+  // } catch (tf2::TransformException& ex) {
+  //   RCLCPP_WARN(this->get_logger(), "Tranformation from %s to 'carla_map' is not available", pose_bl.header.frame_id.c_str());
+  //   return;
+  // }
+
+  // // Transform pose from base_link to carla_map frame
+  // geometry_msgs::msg::PoseStamped pose_map;
+  // tf2::doTransform(pose_bl, pose_map, base_link_to_carla_map_tf);
+
+  // // Set velocity of pose by backward differences
+  // geometry_msgs::msg::Twist twist;
+  // if (!recent_pose_init_) {
+  //   // If no recent pose exists, set velocity to zero (only for very first pose)
+  //   twist.linear.x = 0;
+  //   twist.linear.y = 0;
+  //   twist.linear.z = 0;
+  //   twist.angular.x = 0;
+  //   twist.angular.y = 0;
+  //   twist.angular.z = 0;
+  //   recent_pose_init_ = true;
+  // }
+  // else {
+  //   // Take backwards differences to derive estimated velocites (only set v_x, v_y and yaw rate)
+  //   twist.linear.x = (pose_map.pose.position.x - recent_pose_.position.x) / pub_duration_; // v_x
+  //   twist.linear.y = (pose_map.pose.position.y - recent_pose_.position.y) / pub_duration_; // v_y
+  //   twist.linear.z = 0;
+
+  //   twist.angular.x = 0;
+  //   twist.angular.y = 0;
+  //   double current_roll, current_pitch, current_yaw, recent_roll, recent_pitch, recent_yaw;
+  //   tf2::Quaternion q_current(pose_map.pose.orientation.x, pose_map.pose.orientation.y, pose_map.pose.orientation.z, pose_map.pose.orientation.w);
+  //   tf2::Matrix3x3 m_current(q_current);
+  //   m_current.getRPY(current_roll, current_pitch, current_yaw);
+  //   tf2::Quaternion q_recent(recent_pose_.orientation.x, recent_pose_.orientation.y, recent_pose_.orientation.z, recent_pose_.orientation.w);
+  //   tf2::Matrix3x3 m_recent(q_recent);
+  //   m_recent.getRPY(recent_roll, recent_pitch, recent_yaw);
+  //   twist.angular.z = (current_yaw - recent_yaw) / pub_duration_ ; // yaw rate
+  // }  
+
+  // // Publish pose and twist to carla
+  // pub_pose_->publish(pose_map.pose);
+  // recent_pose_ = pose_map.pose;
+  // pub_twist_->publish(twist);
+  // RCLCPP_DEBUG(this->get_logger(), "Published pose and twist to Carla!");
+}
+
+double SimpleControllerNode::longitudinalControlStep(double current_velocity, double target_velocity)
+{
+  double previous_error = error_long;
+  error_long = target_velocity - current_velocity;
+  // restrict integral term to avoid integral windup
+  error_long_integral = std::max(-40.0, std::min(error_long_integral + error_long, 40.0));
+  error_long_derivative = error_long - previous_error;
+  double output = p_long * error_long + i_long * error_long_integral + d_long * error_long_derivative;
+  return std::max(-1.0, std::min(output, 1.0));
+}
+
+double SimpleControllerNode::lateralControlStep(double current_yaw, double target_yaw)
+{
+  double previous_error = error_lat;
+  error_lat = target_yaw - current_yaw;
+  // restrict integral term to avoid integral windup
+  error_lat_integral = std::max(-400.0, std::min(error_lat_integral + error_lat, 400.0));
+  error_lat_derivative = error_lat - previous_error;
+  double output = p_lat * error_lat + i_lat * error_lat_integral + d_lat * error_lat_derivative;
+  return std::max(-1.0, std::min(output, 1.0));
+
 }
 
 bool SimpleControllerNode::linearInterpolation(const std::vector<double>& X, const std::vector<double>& Y, const double& desired_x, double& output_y)
