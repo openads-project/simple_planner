e61bf9aec8c6f9efa3c000ed1114752ee87ccacf
Jean-Pierre Busch <jean-pierre.busch@ika.rwth-aachen.de>
2025-08-27 10:06:12 +0200

    try better handling of traffic lights

--- src/simple_planner_node.cpp
+++ src/simple_planner_node.cpp
@@ -358,6 +358,11 @@ SimplePath SimplePlannerNode::convertRouteToSimplePath(const route_planning_msgs
         offset_to_stop_line = offset_to_stop_line_ + ego_data_.length / 2.0 + ego_data_.state.reference_point.translation_to_geometric_center.x;
         double dt_offset_to_stop_line = offset_to_stop_line / simple_path_point.v; // time to stop in front of traffic light
 
+        double remaining_distance = tf_route.route_elements[tf_route.current_route_element_idx].s - tf_route.route_elements[j].s - offset_to_stop_line;
+        double v_ego = perception_msgs::object_access::getVelocityMagnitude(ego_data_);
+        double t_remaining = remaining_distance / v_ego;
+
+
         if (reg_elems[k].has_validity_stamp && consider_future_states_) {
           double validity_duration = rclcpp::Time(reg_elems[k].validity_stamp).seconds() - rclcpp::Time(route_.header.stamp).seconds();
           if (validity_duration < (t_total - dt_offset_to_stop_line)) {
@@ -375,6 +380,11 @@ SimplePath SimplePlannerNode::convertRouteToSimplePath(const route_planning_msgs
           }
         } else {
           stop_at_end = true; // no validity stamp or no future states considered, stop required
+        }
+
+        if (t_remaining < 5.0) {
+          RCLCPP_INFO(this->get_logger(), "Traffic light is only %f m away. Don't consider it for stopping.", remaining_distance);
+          stop_at_end = false;
         }
       }
     }
