4b4e042ab03bfa015d01afddbc074c779c4a3519
Guido Linden <guido.kueppers@ika.rwth-aachen.de>
2026-05-08 13:56:43 +0000

    Apply 2 suggestion(s) to 1 file(s)
    Co-authored-by: Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>

--- src/simple_planner_node.cpp
+++ src/simple_planner_node.cpp
@@ -334,8 +334,8 @@ void SimplePlannerNode::setHealth(const unsigned char status, const std::string&
  * @param[in] msg   egoData
  */
 void SimplePlannerNode::egoDataCallback(const perception_msgs::msg::EgoData::UniquePtr msg) {
+  ego_data_topic_diagnostic_->tick(msg→header.stamp);
   ego_data_ = *msg;
-  ego_data_topic_diagnostic_->tick(msg->header.stamp);
 
   if (!ego_data_init_) {
     ego_data_init_ = true;
@@ -358,8 +358,8 @@ void SimplePlannerNode::objectListCallback(const perception_msgs::msg::ObjectLis
  * @param[in] msg   route
  */
 void SimplePlannerNode::routeCallback(const route_planning_msgs::msg::Route::UniquePtr msg) {
-  route_ = *msg;
-  route_topic_diagnostic_->tick(msg->header.stamp);
+  route_topic_diagnostic_->tick(msg→header.stamp);
+  route_ = *msg; 
 
   if (!route_init_) {
     RCLCPP_INFO(this->get_logger(), "Received new route message, initialized global variable");
