369c199815c55103caa31bfc9c0c70909647b21a
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2026-08-24 23:05:41 +0200

    detect non-enriched route

--- simple_planner/src/simple_planner.cpp
+++ simple_planner/src/simple_planner.cpp
@@ -347,6 +347,11 @@ void SimplePlannerNode::routeCallback(const route_planning_msgs::msg::Route::Uni
     route_topic_diagnostic_->tick(msg->header.stamp);
   }
   route_ = *msg;
+  if (!route_.has_route_elements) {
+    RCLCPP_WARN(this->get_logger(), "Received reference-line-only Route on enriched route topic");
+    route_init_ = false;
+    return;
+  }
 
   if (!route_init_) {
     RCLCPP_INFO(this->get_logger(), "Received new route message, initialized global variable");
